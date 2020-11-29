c0f3c280593eeb5b536303683ce4c1c76eecfa87
Alexandre Zanni <azanni001@iutbayonne.univ-pau.fr>
2020-11-29 11:20:00 +0100

    Ajout des commentaires à ProstagesController.php

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -11,28 +11,28 @@ class ProstagesController extends AbstractController
     /**
      * @Route("/", name="prostages_accueil")
      */
-    public function index()
-    {
+    public function index() // La vue index.html.twig affiche le message d'accueil  
+    { 
         return $this->render('prostages/index.html.twig');
     }
 	/**
      * @Route("/entreprises", name="prostages_entreprises")
      */
-    public function entreprises()
+    public function entreprises() // La vue entreprises.html.twig affichera la liste des entreprises
     {
         return $this->render('prostages/entreprises.html.twig');
     }
 	/**
      * @Route("/formations", name="prostages_formations")
      */
-    public function formations()
+    public function formations() // La vue formations.html.twig affichera la liste des formations
     {
         return $this->render('prostages/formations.html.twig');
     }
 	/**
      * @Route("/stages/{id}", name="prostages_stages")
      */
-    public function stages($id)
+    public function stages($id) // La vue stages.html.twig affichera la liste des stages
     {
         return $this->render('prostages/stages.html.twig',
 		[ 'id_stages' => $id]);
