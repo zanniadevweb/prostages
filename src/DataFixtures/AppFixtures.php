749d89055f655783c4c8c6c2a46cf45373d62b7f
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2020-12-29 16:24:36 +0100

    Génération avec une boucle de 15 lignes de données de test par entité

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,47 +16,52 @@ class AppFixtures extends Fixture
         // Création d'un générateur de données Faker en 'français'
          $faker = \Faker\Factory::create('fr_FR');
 
-         $formation = new Formation();
-         $formation->setIntitule($faker->randomElement($array = array ('DUT Informatique','DUT Techniques de commercialisation','DUT Génie industriel et Maintenance', 'LP Métiers du Numérique', 'LP Programmation Avancée', 'Master Informatique', 'Master Humanités Numériques', 'Master Développement Web')));
-         // Génére une ville de formation
-         $villeFormation = $faker->city;
-         $typeEtablissementAccueil = $faker->randomElement($array = array ('IUT de ', 'Université de '));
-         $formation->setEtablissementAccueil($typeEtablissementAccueil.$villeFormation);
-         // Génére un numéro de niveau compris entre 2 et 5.
-         $numeroNiveau = $faker->numberBetween($min = 1, $max = 5);
-         $formation->setNiveau($faker->regexify('BAC\+'.$numeroNiveau));
-         $formation->setVilleFormation($villeFormation);
+
+         // Génération de 15 lignes de données de test pour chaque entité
+        for ($i = 0; $i < 15; $i++) 
+        {
+             $formation = new Formation();
+             $formation->setIntitule($faker->randomElement($array = array ('DUT Informatique','DUT Techniques de commercialisation','DUT Génie industriel et Maintenance', 'LP Métiers du Numérique', 'LP Programmation Avancée', 'Master Informatique', 'Master Humanités Numériques', 'Master Développement Web')));
+             // Génére une ville de formation
+             $villeFormation = $faker->city;
+             $typeEtablissementAccueil = $faker->randomElement($array = array ('IUT de ', 'Université de '));
+             $formation->setEtablissementAccueil($typeEtablissementAccueil.$villeFormation);
+             // Génére un numéro de niveau compris entre 2 et 5.
+             $numeroNiveau = $faker->numberBetween($min = 1, $max = 5);
+             $formation->setNiveau($faker->regexify('BAC\+'.$numeroNiveau));
+             $formation->setVilleFormation($villeFormation);
 
 
-         $entreprise = new Entreprise();
-         $entreprise->setNom($faker->company);
-         $entreprise->setActivite($faker->randomElement($array = array ('Aéronautique','Humanités Numériques','Banque / Assurance', 'Agroalimentaire', 'Electronique / Electricité', 'Industrie pharmaceutique', 'Transport/Logistique', 'Commerce')));
-         $entreprise->setAdresse($faker->address);
-         $entreprise->setTelephone($faker->phoneNumber);
-         $entreprise->setUrl($faker->url);
+             $entreprise = new Entreprise();
+             $entreprise->setNom($faker->company);
+             $entreprise->setActivite($faker->randomElement($array = array ('Aéronautique','Humanités Numériques','Banque / Assurance', 'Agroalimentaire', 'Electronique / Electricité', 'Industrie pharmaceutique', 'Transport/Logistique', 'Commerce')));
+             $entreprise->setAdresse($faker->address);
+             $entreprise->setTelephone($faker->phoneNumber);
+             $entreprise->setUrl($faker->url);
 
 
-         $stage = new Stage();
-         $stage->setEntreprise($entreprise);
-         $stage->addFormation($formation);
-         $stage->setTitre($faker->jobTitle);
-         $stage->setDomaine($faker->randomElement($array = array ('Programmation web','Réseaux','Maintenance informatique', 'Design web', 'Assembleur', 'Programmation bas-niveau', 'Génie Logiciel', 'Maquettage web')));
-         $stage->setDescription($faker->realText($maxNbChars = 40, $indexSize = 2));
-         //$dateDebut = new \Datetime ('30-11-2020'); // Variable qui permet de tester la différence avec la date de fin pour donner la valeur de la durée 
-         //$dateFin = new \Datetime ('11-12-2020'); // Variable qui permet de tester la différence avec la date de début pour donne la valeur de la durée 
-         //$dureeStage = $dateFin-$dateDebut; // Variable donnant la valeur de la différence entre dateDebut et dateFin
-         //$stage->setDuree($dureeStage); 
-         $stage->setDateDebut($faker->dateTime($max = 'now'));
-         $stage->setDateFin($faker->dateTime($max = 'now'));
-         $stage->setEmail($faker->companyEmail);
-         
+             $stage = new Stage();
+             $stage->setEntreprise($entreprise);
+             $stage->addFormation($formation);
+             $stage->setTitre($faker->jobTitle);
+             $stage->setDomaine($faker->randomElement($array = array ('Programmation web','Réseaux','Maintenance informatique', 'Design web', 'Assembleur', 'Programmation bas-niveau', 'Génie Logiciel', 'Maquettage web')));
+             $stage->setDescription($faker->realText($maxNbChars = 40, $indexSize = 2));
+             //$dateDebut = new \Datetime ('30-11-2020'); // Variable qui permet de tester la différence avec la date de fin pour donner la valeur de la durée 
+             //$dateFin = new \Datetime ('11-12-2020'); // Variable qui permet de tester la différence avec la date de début pour donne la valeur de la durée 
+             //$dureeStage = $dateFin-$dateDebut; // Variable donnant la valeur de la différence entre dateDebut et dateFin
+             //$stage->setDuree($dureeStage); 
+             $stage->setDateDebut($faker->dateTime($max = 'now'));
+             $stage->setDateFin($faker->dateTime($max = 'now'));
+             $stage->setEmail($faker->companyEmail);
+             
 
-         // Enregistrement de la formation créée
-         $manager->persist($formation);
-         // Enregistrement de l'entreprise créée
-         $manager->persist($entreprise);    
-         // Enregistrement du stage créé
-         $manager->persist($stage);
+             // Enregistrement de la formation créée
+             $manager->persist($formation);
+             // Enregistrement de l'entreprise créée
+             $manager->persist($entreprise);    
+             // Enregistrement du stage créé
+             $manager->persist($stage);
+        }
 
 
          // Envoyer les données en base de données
