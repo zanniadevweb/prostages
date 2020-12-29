f9552b2167198448cb16eae4a1a6ed001df39fe0
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2020-12-29 16:18:45 +0100

    Ajout ligne permettant d'utiliser faker. Ajout de données de tests générées par la librairie Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Stage;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
+use Faker;
 
 class AppFixtures extends Fixture
 {
@@ -16,38 +17,49 @@ class AppFixtures extends Fixture
          $faker = \Faker\Factory::create('fr_FR');
 
          $formation = new Formation();
-         $formation->setIntitule("DUT Informatique");
-         $formation->setEtablissementAccueil("IUT de Bayonne et du Pays Basque");
-         $formation->setNiveau("BAC+2");
+         $formation->setIntitule($faker->randomElement($array = array ('DUT Informatique','DUT Techniques de commercialisation','DUT Génie industriel et Maintenance', 'LP Métiers du Numérique', 'LP Programmation Avancée', 'Master Informatique', 'Master Humanités Numériques', 'Master Développement Web')));
+         // Génére une ville de formation
+         $villeFormation = $faker->city;
+         $typeEtablissementAccueil = $faker->randomElement($array = array ('IUT de ', 'Université de '));
+         $formation->setEtablissementAccueil($typeEtablissementAccueil.$villeFormation);
+         // Génére un numéro de niveau compris entre 2 et 5.
+         $numeroNiveau = $faker->numberBetween($min = 1, $max = 5);
+         $formation->setNiveau($faker->regexify('BAC\+'.$numeroNiveau));
+         $formation->setVilleFormation($villeFormation);
+
 
          $entreprise = new Entreprise();
-         $entreprise->setNom("Price Induction");
-         $entreprise->setActivite("Aéronautique");
-         $entreprise->setAdresse("Esplanade de l'Europe, 64600 Anglet");
-         $entreprise->setTelephone("08.92.97.62.21");
-         $entreprise->setUrl("https://www.societe.com/societe/price-induction-325131167.html");
+         $entreprise->setNom($faker->company);
+         $entreprise->setActivite($faker->randomElement($array = array ('Aéronautique','Humanités Numériques','Banque / Assurance', 'Agroalimentaire', 'Electronique / Electricité', 'Industrie pharmaceutique', 'Transport/Logistique', 'Commerce')));
+         $entreprise->setAdresse($faker->address);
+         $entreprise->setTelephone($faker->phoneNumber);
+         $entreprise->setUrl($faker->url);
+
 
          $stage = new Stage();
          $stage->setEntreprise($entreprise);
          $stage->addFormation($formation);
-         $stage->setTitre("Refonte et mise à jour du site web de ProStage");
-         $stage->setDomaine("web");
-         $stage->setDescription("Etude du site existant - Prise en compte des mises à jour à effectuer");
-         $dateDebut = new \Datetime ('30-11-2020'); // Variable qui permet de tester la différence avec la date de fin pour donner la valeur de la durée 
-         $dateFin = new \Datetime ('11-12-2020'); // Variable qui permet de tester la différence avec la date de début pour donne la valeur de la durée 
+         $stage->setTitre($faker->jobTitle);
+         $stage->setDomaine($faker->randomElement($array = array ('Programmation web','Réseaux','Maintenance informatique', 'Design web', 'Assembleur', 'Programmation bas-niveau', 'Génie Logiciel', 'Maquettage web')));
+         $stage->setDescription($faker->realText($maxNbChars = 40, $indexSize = 2));
+         //$dateDebut = new \Datetime ('30-11-2020'); // Variable qui permet de tester la différence avec la date de fin pour donner la valeur de la durée 
+         //$dateFin = new \Datetime ('11-12-2020'); // Variable qui permet de tester la différence avec la date de début pour donne la valeur de la durée 
          //$dureeStage = $dateFin-$dateDebut; // Variable donnant la valeur de la différence entre dateDebut et dateFin
          //$stage->setDuree($dureeStage); 
-         $stage->setDateDebut($dateDebut);
-         $stage->setDateFin($dateFin);
-         $stage->setEmail("john.doe@johndoe.com");
+         $stage->setDateDebut($faker->dateTime($max = 'now'));
+         $stage->setDateFin($faker->dateTime($max = 'now'));
+         $stage->setEmail($faker->companyEmail);
          
 
+         // Enregistrement de la formation créée
          $manager->persist($formation);
+         // Enregistrement de l'entreprise créée
          $manager->persist($entreprise);    
+         // Enregistrement du stage créé
          $manager->persist($stage);
 
 
-
+         // Envoyer les données en base de données
         $manager->flush();
     }
 }
