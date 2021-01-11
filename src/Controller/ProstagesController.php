984043bd365a009cc4f39591f44fe739380c9103
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2021-01-11 18:11:54 +0100

    fin tp4 : Réorganisation du code du contrôleur métier avant l'injection de dépendances

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -12,59 +12,104 @@ use App\Entity\Stage;
 
 class ProstagesController extends AbstractController
 {
+
     /**
      * @Route("/", name="prostages_accueil")
      */
     public function index() // La vue index.html.twig affiche le message d'accueil  
     { 
-        $stages = $this->getDoctrine()->getRepository(Stage::class)->findAll();
+        // Récupérer le repositery de l'entité Stage
+        $repositeryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+        // Récupérer les informations générales des stages enregistrés en BD
+        $stages = $repositeryStage->findAll();
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/index.html.twig',
         [ 'stages' => $stages ]);
     }
+
+
     /**
      * @Route("/entreprises", name="prostages_entreprises")
      */
     public function entreprises() // La vue entreprises.html.twig affichera la liste des entreprises
     {
-        $entreprises = $this->getDoctrine()->getRepository(Entreprise::class)->findAll();
+        // Récupérer le repositery de l'entité Entreprise
+        $repositeryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+
+        // Récupérer les informations des entreprises enregistrées en BD
+        $Entreprises = $repositeryEntreprise->findAll();
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/entreprises.html.twig',
-        [ 'entreprises' => $entreprises ]);
+        [ 'entreprises' => $Entreprises ]);
     }
+
+
     /**
      * @Route("/entreprise/{id}/stages", name="prostages_entreprises_stage")
      */
     public function getByEntreprise($id) // La vue affichera la liste des stages proposés par une entreprise
     {
-        $entreprise = $this->getDoctrine()->getRepository(Entreprise::class)->find($id);
-        $stages = $entreprise->getStages();
+        // Récupérer le repositery de l'entité Entreprise
+        $repositeryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+
+        // Récupérer les informations des stages enregistrés en BD
+        $stages = $repositeryEntreprise->find($id);
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/index.html.twig',
-        [ 'stages' => $stages ]);
+        [ 'stages' => $stages->getStages() ]);
     }
+
+
     /**
      * @Route("/formations", name="prostages_formations")
      */
     public function formations() // La vue formations.html.twig affichera la liste des formations
     {
-        $formations = $this->getDoctrine()->getRepository(Formation::class)->findAll();
+        // Récupérer le repositery de l'entité Formation
+        $repositeryFormations = $this->getDoctrine()->getRepository(Formation::class);
+
+        // Récupérer les informations des formations enregistrées en BD
+        $formations = $repositeryFormations->findAll();
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/formations.html.twig',
         [ 'formations' => $formations ]);
     }
+
+
     /**
      * @Route("/formation/{id}/stages", name="prostages_formations_stage")
      */
     public function getByFormation($id) // La vue affichera la liste des stages proposés pour une formation
     {
-        $formation = $this->getDoctrine()->getRepository(Formation::class)->find($id);
-        $stages = $formation->getStages();
+        // Récupérer le repositery de l'entité Formation
+        $repositeryFormation = $this->getDoctrine()->getRepository(Formation::class);
+
+        // Récupérer les informations des stages enregistrés en BD
+        $stages = $repositeryFormation->find($id);
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/index.html.twig',
-        [ 'stages' => $stages ]);
+        [ 'stages' => $stages->getStages() ]);
     }
+
+
     /**
      * @Route("/stages/{id}", name="prostages_stages")
      */
     public function stages($id) // La vue stages.html.twig affichera la liste des stages
     {
-        $stage = $this->getDoctrine()->getRepository(Stage::class)->find($id);
+        // Récupérer le repositery de l'entité Stage
+        $repositeryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+        // Récupérer les informations détaillées des stages (par l'id) enregistrés en BD
+        $stage = $repositeryStage->find($id);
+
+        // Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('prostages/stages.html.twig',
         [ 'stage' => $stage ]);
     }
